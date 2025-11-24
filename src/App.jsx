ce1a0dca46a06d1d3bbdd367f37bd720504b220d
ra-eesa <zareesa12@gmail.com>
2025-11-24 21:51:29 +0000

    update: contact info

--- src/App.jsx
+++ src/App.jsx
@@ -206,7 +206,7 @@ function App() {
               href="mailto:hello@kabirtechnologies.com"
               className="inline-block px-8 py-4 bg-gradient-to-r from-kt-gold-light to-kt-gold text-white font-medium rounded-xl hover:shadow-lg hover:shadow-kt-gold/30 hover:scale-105 transition-all duration-200"
             >
-              hello@kabirtechnologies.com
+              contact information coming soon
             </a>
           </div>
         </section>
